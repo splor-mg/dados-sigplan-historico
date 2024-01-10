58cc739fcc2b1ef01107a47cccf47dec631e5e70
Francisco Júnior <fjunior.alves.oliveira@gmail.com>
2024-01-09 21:48:41 -0300

    checkpoint

--- scripts/render_template.py
+++ scripts/render_template.py
@@ -1,4 +1,4 @@
-from jinja2 import Environment, PackageLoader
+from jinja2 import Environment, FileSystemLoader
 
 def render_schema_planejamento(package_name, resource_name, period):
     """
@@ -6,7 +6,7 @@ def render_schema_planejamento(package_name, resource_name, period):
 
     >>> render_schema_planejamento("planejamento", "acoes_planejamento", 2023)
     """
-    env = Environment(loader=PackageLoader("main"), keep_trailing_newline = True)
+    env = Environment(loader=FileSystemLoader("templates"), keep_trailing_newline = True)
     template = env.get_template(f"{resource_name}.yaml.j2")
     result = template.render(period = period)
     with open(f"datapackages/{package_name}{period}/schemas/{resource_name}.yaml", 'w') as fs:
@@ -18,7 +18,7 @@ def render_schema_monitoramento(package_name, resource_name, period, months):
 
     >>> render_schema_monitoramento("monitoramento", "acoes_monitoramento", 2020, months = ["Jan", "Fev", "Mar"])
     """
-    env = Environment(loader=PackageLoader("main"),
+    env = Environment(loader=FileSystemLoader("templates"),
                       trim_blocks=True,
                       lstrip_blocks=True,
                       keep_trailing_newline=True)
@@ -28,11 +28,11 @@ def render_schema_monitoramento(package_name, resource_name, period, months):
         fs.write(result)
 
 render_schema_monitoramento("monitoramento", 
-                            "acoes_monitoramento", 2022, 
+                            "acoes_monitoramento", 2020, 
                             months = ["Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Out", "Nov", "Dez"])
 
 render_schema_monitoramento("monitoramento", 
-                            "localizadores_monitoramento_todos", 2022, 
+                            "localizadores_monitoramento_todos", 2020, 
                             months = ["Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Out", "Nov", "Dez"])
 
 
