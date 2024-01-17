f2a7061516e2c3b3e9b507c7eea5d68deeb072ee
Francisco Júnior <fjunior.alves.oliveira@gmail.com>
2024-01-17 10:33:08 -0300

    exploratory data analysis scripts

--- scripts/export.R
+++ scripts/export.R
@@ -1,4 +1,31 @@
+library(frictionless)
 library(data.table)
+library(dplyr)
 
-acoes_monitoramento <- fread("data/acoes_monitoramento.csv")
-indicadores_planejamento <- fread("data/indicadores_planejamento.csv")
+package <- read_package("datapackage.json")
+
+acoes_monitoramento <- read_resource(package, "acoes_monitoramento")
+
+acoes_monitoramento |> 
+  select(uo_acao_cod,
+         uo_acao_nome,
+         programa_cod,
+         programa_desc,
+         acao_cod,
+         acao_desc,
+         produto_desc,
+         vlr_credito_inicial,
+         vlr_credito_autorizado,
+         vlr_despesa_realizada,
+         vlr_prog_fisica_inicial, # this is not comparable between bimesters
+         vlr_previsao_fisica,
+         vlr_fisico_realizado, 
+         ppag) |> 
+  filter(acao_cod == 4428)
+
+indicadores_planejamento <- read_resource(package, "indicadores_planejamento")
+
+indicadores_planejamento |> 
+  filter()
+
+
